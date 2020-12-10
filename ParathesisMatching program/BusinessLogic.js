46ec36b828738fb4fb083ca40925671e84c18f7b
vijay11234 <70164574+vijay11234@users.noreply.github.com>
2020-12-10 16:31:35 +0530

    Added: ParathesisMatching program

--- ParathesisMatching program/BusinessLogic.js
+++ ParathesisMatching program/BusinessLogic.js
@@ -11,7 +11,7 @@ class BusinessLogic {
             if (str[i] === '(' || str[i] === '{' || str[i] === '[') {
                 stack.push(str[i]);
             }
-            //if that char is a closing brace, pop from the stack which will also reduce the length of the stack each time a closing bracket is encountered
+            //if that char is a closing brace, pop from the stack 
             else {
                 let last = stack.pop();
                 //if the pooped elemet from the stack, which is the last opening brace doesn't match the corresponding closing brace in the map, then return false
@@ -20,7 +20,7 @@ class BusinessLogic {
                 }
             }
         }
-        // by the completion of the for loop after checking all the brack of the str, at the end, if the stack is not empty then fail
+        // by the completion of the for loop after checking all the brack of the str
         if (stack.length !== 0) {
             return "Not Balanced";
         }
